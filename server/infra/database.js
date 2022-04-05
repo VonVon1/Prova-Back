3a1a8c32387b644519455217c4446d733b2b02fc
root <root@DESKTOP-VVG8S4C.localdomain>
2022-04-05 17:27:36 -0300

    apagando a dbport

--- server/infra/database.js
+++ server/infra/database.js
@@ -3,8 +3,9 @@ const db = pgp({
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
 	database: process.env.DN_NAME
 });
 
 module.exports = db;
+
+console.log(process.env.DB_USER);
